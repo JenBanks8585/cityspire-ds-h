c0b63cc0c6f99099203acc52c6dc3b6b67d05908
JenBanks8585 <jennifer.banks8585@gmail.com>
2021-02-02 22:54:04 -0500

    added import in config.py and external.py router on main

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app import db, ml, viz
+from app import db, ml, viz, external
 
 description = """
 Edit your app's title and description. See [https://fastapi.tiangolo.com/tutorial/metadata/](https://fastapi.tiangolo.com/tutorial/metadata/)
@@ -24,6 +24,7 @@ app = FastAPI(
 app.include_router(db.router, tags=['Database'])
 app.include_router(ml.router, tags=['Machine Learning'])
 app.include_router(viz.router, tags=['Visualization'])
+app.include_router(external.router, tags=['External Resources'])
 
 app.add_middleware(
     CORSMiddleware,
